f696e0c47faefaa7e9b8e401e04e558e3ef15a30
EtherealManifest <richmondjacobg@gmail.com>
2023-10-09 11:30:24 -0500

    Added requirements file

--- main.py
+++ main.py
@@ -6,23 +6,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import pprint
+import prettyprint
 from pathlib import Path
 
 '''
 
-(10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is
- labeled tabular data.
-(10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build
-some fancy charts here as it will greatly help you in future homework assignments and in the final project.
-(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the
- project should save these when it executes. You may want to add this folder to your .gitignore file.
-(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
-(10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt
+10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+(10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt.txt
 file which contains all the packages that need installed. You can create this file with the output of pip freeze at the
  terminal prompt.
 (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the
- pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the
+ pip requirements.txt, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the
 explanations.
 '''
 
